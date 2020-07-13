19c59c7977ca0ae5261be9be0143b89272d161a9
venturads <albert@venturads.com>
2020-07-13 00:22:24 -0700

    trans to translist

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,17 +2,16 @@ import React, {useContext} from "react";
 import {GlobalContext} from '../context/GlobalState';
 
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
+    const {transactions} = useContext(GlobalContext);
 
-    console.log(context);
-    
     return (
         <>
             <h3>History</h3>
             <ul className="list">
+            {transactions.map(transaction => (
                 <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+                </li>))}
             </ul>
         </>
     )
