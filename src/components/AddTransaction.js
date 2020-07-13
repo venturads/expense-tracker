4d1fe452b428eed17a8f27b0269a69ee715a52f3
venturads <albert@venturads.com>
2020-07-12 23:46:51 -0700

    add state to addtrans form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const TransactionList = () => {
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
     return (
         <>
             <h3>Add new transaction</h3>
             <form>
                 <div className="form-control">
-                <label htmlfor="text">Text</label>
-                <input type="text" className="text" placeholder="Enter text..." />
+                    <label htmlfor="text">Text</label>
+                    <input type="text"
+                    value={text} onChange={(e) => setText(e.target.value)}
+                     className="text" placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                <label htmlfor="amount"
+                    <label htmlfor="amount"
                     >Amount <br />
                     (negative - expense, positive - income)</label
                 >
-                <input type="number" className="amount" placeholder="Enter amount..." />
+                    <input type="number" 
+                    value={amount} onChange={(e) => setAmount(e.target.value)}
+                    className="amount" placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
