232993917991d9660f6d7fea2dc34d6fa4e1ebfc
venturads <albert@venturads.com>
2020-07-13 11:59:14 -0700

    Updates

---
+++ precache-manifest.5d2e671dad3b7163f39b4445d53f4219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4189a701b2b717a7204d03d7a25ad91",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "4708ddcc619a4881eb3f",
+    "url": "/expense-tracker/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "9857be389dfb347fd6f9",
+    "url": "/expense-tracker/static/js/2.2c5e5d68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.2c5e5d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4708ddcc619a4881eb3f",
+    "url": "/expense-tracker/static/js/main.35511328.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
